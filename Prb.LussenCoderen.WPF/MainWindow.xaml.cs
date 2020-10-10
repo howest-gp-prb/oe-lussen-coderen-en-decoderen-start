303b37b184d09c270e6a27bcc9d542b854570750
jandedeuraerder-howest <jan.de.deurwaerder@howest.be>
2020-10-10 14:16:58 +0200

    Add project files.

---
+++ Prb.LussenCoderen.WPF/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Prb.LussenCoderen.WPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnEncodeSimple_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnDecodeSimple_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnEncodeComplex_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnDecodeComplex_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
